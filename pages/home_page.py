26b7fe1f1d423bcd1b83d0e6b2d76f3070a66b8b
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2020-03-29 21:06:07 -0300

    Using by for more clear writing

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,8 @@
+from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
 class HomePage(BasePage):
+    
     @staticmethod
     def search_input(driver):
-        return driver.find_element_by_name('q')
+        return driver.find_element(By.NAME, 'q')
